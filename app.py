45300e2d8311e34413090b4a2238f755e3c42342
rajguru121 <tanmayrajguru007@gmail.com>
2026-04-18 10:36:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -976,4 +976,4 @@ def history(session_id=None):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)), debug=True)
